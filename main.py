f0c383ab2ab75f28f895abaac5d248c73c663009
KirillSPC <kirillsolomkopoland@example.com>
2023-10-26 16:56:10 +0200

    Home_10

--- main.py
+++ main.py
@@ -1,1 +1,76 @@
 from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        if not self.is_valid_phone(value):
+            raise ValueError("Invalid phone number format")
+        super().__init__(value)
+
+    @staticmethod
+    def is_valid_phone(value):
+        return len(value) == 10 and value.isdigit()
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        phone_to_remove = None
+        for p in self.phones:
+            if p.value == phone:
+                phone_to_remove = p
+                break
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+        else:
+            raise ValueError("Phone number not found")
+
+    def edit_phone(self, old_phone, new_phone):
+        phone_to_edit = None
+        for p in self.phones:
+            if p.value == old_phone:
+                phone_to_edit = p
+                break
+        if phone_to_edit:
+            phone_to_edit.value = new_phone
+        else:
+            raise ValueError("Phone number to edit not found")
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        if name in self.data:
+            return self.data[name]
+        return None
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+
